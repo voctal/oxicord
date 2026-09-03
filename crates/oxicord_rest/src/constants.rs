1abd75f1b3e367476dfb1907b47a4297d4aab384
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-09-03 03:37:54 +0200

    fix: remove version from the base url

--- crates/oxicord_rest/src/constants.rs
+++ crates/oxicord_rest/src/constants.rs
@@ -2,4 +2,4 @@
 pub const API_VERSION: u8 = 10;
 
 /// Base URL for the Discord REST API.
-pub const BASE_URL: &str = "https://discord.com/api/v10";
+pub const BASE_URL: &str = "https://discord.com/api";
